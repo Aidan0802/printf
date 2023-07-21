5b1d160ab54a430e43f3b380a1fed31a2ab69747
aidan <aidanheydenrycht@gmail.com>
2023-07-21 16:49:20 +0300

    created header and c file

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+int _putchar(int c);
+int _printf(const char *format, ...);
+
+#endif
---
+++ printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _printf - prints output according to format
+ *
+ * @format: format that is printed out
+ *
+ * Return: The number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i, j, count = 0;
+	char *str;
+	va_list args;
+
+	va_start(args, format);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+				_putchar(va_arg(args, int));
+
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				for (j = 0; str[j]; j++)
+					_putchar(str[j]);
+			}
+		}
+		else
+			_putchar(format[i]);
+	}
+
+	return (count);
+}
+
+/**
+ * _putchar - prints a single char
+ *
+ * @c: char input
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(int c)
+{
+	return (write(1, &c, 1));
+}
