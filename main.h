748f7db3589c88607692629720ba9847a08b6830
aidan <aidanheydenrycht@gmail.com>
2023-07-24 22:53:24 +0300

    [Updated] header

--- main.h
+++ main.h
@@ -3,16 +3,7 @@
 
 #include <stdarg.h>
 #include <unistd.h>
-
-
-int _putchar(int c);
-int _printf(const char *format, ...);
-int int_print(va_list);
-int (*get_spes(const char *, int))(va_list);
-int _str(va_list);
-int _c(va_list);
-int _perc(va_list);
-int get_int(int);
+#include <stdlib.h>
 
 /**
  * struct sp - structures specifiers
@@ -26,5 +17,14 @@ typedef struct sp
 	char *choice;
 	int (*f)(va_list args);
 } select;
+
+int _putchar(int c);
+int _printf(const char *format, ...);
+int int_print(va_list);
+int (*get_spes(const char *, int))(va_list);
+int _str(va_list);
+int _c(va_list);
+int _perc(va_list);
+int get_int(int);
 
 #endif
