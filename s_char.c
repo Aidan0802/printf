6cae26f4a266ea9b7425386037cff4d863a891e6
aidan <aidanheydenrycht@gmail.com>
2023-07-28 12:28:16 +0300

    [Created] c funtion

---
+++ s_char.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * s_char - print special char
+ *
+ * @args: argument list
+ *
+ * Return: Amount of char printed
+ */
+
+int s_char(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = -1, i, x;
+	char *special = "\\xA0";
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == '\n')
+			for (x = 0; special[x]; x++)
+				_putchar(special[x]), count++;
+		else
+			_putchar(str[i]), count++;
+	}
+	if (count == -1)
+		return (0);
+	return (count);
+}
